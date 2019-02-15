cbd0b25318cc4edd8cc7fc395e641037b10213dd
Jason Molloy <jmolloy619@gmail.com>
2019-02-15 14:25:22 -0700

    working to basic specs - first commit

---
+++ app/main.js
@@ -0,0 +1,12 @@
+import ItunesController from "./components/Itunes/itunesController.js";
+
+
+class App {
+  constructor() {
+    this.controllers = {
+      itunesCtrl: new ItunesController()
+    }
+  }
+}
+
+window["app"] = new App()
---
+++ app/models/song.js
@@ -0,0 +1,32 @@
+export default class Song {
+  constructor(song) {
+    this.title = song.trackName
+    this.albumArt = song.artworkUrl60.replace(/60x60/g, "250x250")
+    this.artist = song.artistName
+    this.collection = song.collectionName
+    this.price = song.collectionPrice
+    this.preview = song.previewUrl
+  }
+
+
+  getTemplate() {
+    return `
+        <div class="col-12">
+          <div class="row d-flex">
+            <img src="${this.albumArt}">
+            <h3 class="songTitle">${this.title} - ${this.artist}</h3>
+          </div>
+          <div class="row">
+            <div class="col-10">
+              <h5>Album: ${this.collection} - Download Album: ${this.price}</h5>
+            </div>
+            <div class="col-2">
+              <audio controls>
+                <source src="${this.preview}">
+              </audio>
+            </div>
+          </div>
+        </div>
+  `
+  }
+}
